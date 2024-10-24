e9bf95e4164dabfaf68087602c36db09c6776624
opeleg24 <opeleg1990@gmail.com>
2024-10-24 15:16:43 +0300

    Upload changes - API

--- test_cases/test_api.py
+++ test_cases/test_api.py
@@ -10,8 +10,14 @@ from workflows.api_flows import APIFlows
 @pytest.mark.usefixtures('init_api')
 class Test_API:
 
-    @allure.title("Test01: Verify initial amount of items & products in header display")
-    @allure.description("This test verifies the initial amount of items & products in the header display")
+    @allure.title("Test01: Verify initial product name")
+    @allure.description("This test verifies the name of the first product in the API response.")
     def test_verify_initial_header_amount(self):
         name_of_product = APIFlows.get_first_product()
-        Verifications.verify_equals(name_of_product, get_data("PRODUCT_NAMR_API"))
+        Verifications.verify_equals(name_of_product, get_data("PRODUCT_NAME_API"))
+
+    @allure.title("Test02: Verify amount of products")
+    @allure.description("This test verifies the amount of prodcuts")
+    def test_verify_amount_of_products(self):
+        amount_of_items = APIFlows.get_amount_of_items()
+        Verifications.verify_equals(amount_of_items, int(get_data("AMOUNT_OF_PRODUCTS_API")))
--- workflows/api_flows.py
+++ workflows/api_flows.py
@@ -10,3 +10,9 @@ class APIFlows:
         response_json = response.json()
         name = response_json[0]['name']
         return name
+
+    @staticmethod
+    def get_amount_of_items():
+        response = APIActions.get(base.request_context)
+        response_json = response.json()
+        return len(response_json)
